92e333ec05b7e70a66e50244c694711dad77bffb
HouariZegai <houarizegai14@gmail.com>
2020-01-10 17:10:40 +0100

    Update some modules

--- src/main/java/com/houarizegai/templateemailsender/App.java
+++ src/main/java/com/houarizegai/templateemailsender/App.java
@@ -1,7 +1,5 @@
 package com.houarizegai.templateemailsender;
 
-import com.houarizegai.templateemailsender.enums.Tags;
-import com.houarizegai.templateemailsender.nodes.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,6 +9,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class App extends Application {
+    public static Stage stage;
     @Override
     public void start(Stage stage) {
         try {
@@ -20,34 +19,12 @@ public class App extends Application {
             ioe.printStackTrace();
         }
 
+        this.stage = stage;
         stage.setTitle("Template Mails Sender");
         stage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    public static String getTemplate() {
-        Header header = new Header("null", "JavaFX Course");
-        Footer footer = new Footer("Houari ZEGAI, JavaFX Instructor",
-                new Link("GitHub", "https://github.com/houarizegai"),
-                new Link("Twitter", "https://twitter.com/houarizegai"));
-
-        Body body = new Body();
-
-        List ol = new List(true,"HTML", "CSS", "JS");
-
-        List ul = new List(false);
-        ul.addAll("Java", "Python");
-        ul.add("JavaScript");
-
-        body.addAll(ol, ul, Tags.BR,
-                new Text("Visit my GitHub at: "),
-                new Link("GitHub Profile", "https://github.com/houarizegai"),
-                Tags.BR, Tags.HR, new Text("Testing"));
-
-        FullPage fullPage = new FullPage(header, body, footer);
-        return String.valueOf(fullPage);
     }
 }
